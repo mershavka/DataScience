08f8208bed62cf5f430a0c17b2f94b0f6cbeeba8
mershavka <mershavkaa@yandex.ru>
2022-12-15 21:15:42 +0300

    Задача на использование бинарного поиска

---
+++ Python_contests/binary_search.py
@@ -0,0 +1,31 @@
+"""В первой строке даны целое число 1≤n≤10^5 и массив A[1…n] из n различных натуральных чисел,
+не превышающих 10^9, в порядке возрастания, во второй — целое число 1≤k≤10^5
+и k натуральных чисел b_1,…,b_k, не превышающих 10^9. Для каждого i от 1 до k
+необходимо вывести индекс 1≤j≤n, для которого A[j]=b_i, или -1, если такого j нет."""
+import math
+def binary_search(value, sorted_list):
+    index = -1
+    left = 0
+    right = len(sorted_list) - 1
+    while left <= right:
+        mean = math.floor(left + (right - left)/2)
+        if sorted_list[mean] == value:
+            index = mean
+            break
+        elif sorted_list[mean] < value:
+            left = mean + 1
+        else:
+            right = mean - 1
+    index = index + 1 if index >= 0 else index
+    return index
+
+def main():
+    n, *A = map(int, input().split())
+    k, *B = map(int, input().split())
+    result = []
+    for element in B:
+        result.append(binary_search(element, A))
+    print(*result)
+
+if __name__ == "__main__":
+    main()
